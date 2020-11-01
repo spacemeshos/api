128a2c5f1d42aebd1348d63d0f42a3926e9826ab
Aviv Eyal <aviveyal07@gmail.com>
2020-11-01 15:26:42 +0200

    Add accounts method to global state api

--- release/go/spacemesh/v1/global_state.pb.gw.go
+++ release/go/spacemesh/v1/global_state.pb.gw.go
@@ -15,6 +15,7 @@ import (
 
 	"github.com/golang/protobuf/descriptor"
 	"github.com/golang/protobuf/proto"
+	"github.com/golang/protobuf/ptypes/empty"
 	"github.com/grpc-ecosystem/grpc-gateway/runtime"
 	"github.com/grpc-ecosystem/grpc-gateway/utilities"
 	"google.golang.org/grpc"
@@ -95,6 +96,40 @@ func local_request_GlobalStateService_Account_0(ctx context.Context, marshaler r
 	}
 
 	msg, err := server.Account(ctx, &protoReq)
+	return msg, metadata, err
+
+}
+
+func request_GlobalStateService_Accounts_0(ctx context.Context, marshaler runtime.Marshaler, client GlobalStateServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq empty.Empty
+	var metadata runtime.ServerMetadata
+
+	newReader, berr := utilities.IOReaderFactory(req.Body)
+	if berr != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
+	}
+	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := client.Accounts(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
+func local_request_GlobalStateService_Accounts_0(ctx context.Context, marshaler runtime.Marshaler, server GlobalStateServiceServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq empty.Empty
+	var metadata runtime.ServerMetadata
+
+	newReader, berr := utilities.IOReaderFactory(req.Body)
+	if berr != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
+	}
+	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := server.Accounts(ctx, &protoReq)
 	return msg, metadata, err
 
 }
@@ -209,6 +244,26 @@ func RegisterGlobalStateServiceHandlerServer(ctx context.Context, mux *runtime.S
 		}
 
 		forward_GlobalStateService_Account_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
+	mux.Handle("POST", pattern_GlobalStateService_Accounts_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateIncomingContext(ctx, mux, req)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := local_request_GlobalStateService_Accounts_0(rctx, inboundMarshaler, server, req, pathParams)
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_GlobalStateService_Accounts_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
 
 	})
 
@@ -333,6 +388,26 @@ func RegisterGlobalStateServiceHandlerClient(ctx context.Context, mux *runtime.S
 
 	})
 
+	mux.Handle("POST", pattern_GlobalStateService_Accounts_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateContext(ctx, mux, req)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_GlobalStateService_Accounts_0(rctx, inboundMarshaler, client, req, pathParams)
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_GlobalStateService_Accounts_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	mux.Handle("POST", pattern_GlobalStateService_AccountDataQuery_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
@@ -381,6 +456,8 @@ var (
 
 	pattern_GlobalStateService_Account_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"v1", "globalstate", "account"}, "", runtime.AssumeColonVerbOpt(true)))
 
+	pattern_GlobalStateService_Accounts_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"v1", "globalstate", "accounts"}, "", runtime.AssumeColonVerbOpt(true)))
+
 	pattern_GlobalStateService_AccountDataQuery_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"v1", "globalstate", "accountdataquery"}, "", runtime.AssumeColonVerbOpt(true)))
 
 	pattern_GlobalStateService_SmesherDataQuery_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"v1", "globalstate", "smesherdataquery"}, "", runtime.AssumeColonVerbOpt(true)))
@@ -390,6 +467,8 @@ var (
 	forward_GlobalStateService_GlobalStateHash_0 = runtime.ForwardResponseMessage
 
 	forward_GlobalStateService_Account_0 = runtime.ForwardResponseMessage
+
+	forward_GlobalStateService_Accounts_0 = runtime.ForwardResponseMessage
 
 	forward_GlobalStateService_AccountDataQuery_0 = runtime.ForwardResponseMessage
 
