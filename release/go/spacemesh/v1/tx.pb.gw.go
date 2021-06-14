71027208c90fe773c7abe168c8ca5974c8f55f49
avive <aviveyal07@gmail.com>
2021-06-14 18:41:57 +0300

    Remove all google.rpc.Status data items - each response has it built-in

--- release/go/spacemesh/v1/tx.pb.gw.go
+++ release/go/spacemesh/v1/tx.pb.gw.go
@@ -99,31 +99,6 @@ func local_request_TransactionService_TransactionsState_0(ctx context.Context, m
 
 }
 
-func request_TransactionService_TransactionsStateStream_0(ctx context.Context, marshaler runtime.Marshaler, client TransactionServiceClient, req *http.Request, pathParams map[string]string) (TransactionService_TransactionsStateStreamClient, runtime.ServerMetadata, error) {
-	var protoReq TransactionsStateStreamRequest
-	var metadata runtime.ServerMetadata
-
-	newReader, berr := utilities.IOReaderFactory(req.Body)
-	if berr != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
-	}
-	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
-	}
-
-	stream, err := client.TransactionsStateStream(ctx, &protoReq)
-	if err != nil {
-		return nil, metadata, err
-	}
-	header, err := stream.Header()
-	if err != nil {
-		return nil, metadata, err
-	}
-	metadata.HeaderMD = header
-	return stream, metadata, nil
-
-}
-
 // RegisterTransactionServiceHandlerServer registers the http handlers for service TransactionService to "mux".
 // UnaryRPC     :call TransactionServiceServer directly.
 // StreamingRPC :currently unsupported pending https://github.com/grpc/grpc-go/issues/906.
@@ -167,13 +142,6 @@ func RegisterTransactionServiceHandlerServer(ctx context.Context, mux *runtime.S
 
 		forward_TransactionService_TransactionsState_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
 
-	})
-
-	mux.Handle("POST", pattern_TransactionService_TransactionsStateStream_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
-		err := status.Error(codes.Unimplemented, "streaming calls are not yet supported in the in-process transport")
-		_, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
-		runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
-		return
 	})
 
 	return nil
@@ -257,26 +225,6 @@ func RegisterTransactionServiceHandlerClient(ctx context.Context, mux *runtime.S
 
 	})
 
-	mux.Handle("POST", pattern_TransactionService_TransactionsStateStream_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
-		ctx, cancel := context.WithCancel(req.Context())
-		defer cancel()
-		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
-		rctx, err := runtime.AnnotateContext(ctx, mux, req)
-		if err != nil {
-			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
-			return
-		}
-		resp, md, err := request_TransactionService_TransactionsStateStream_0(rctx, inboundMarshaler, client, req, pathParams)
-		ctx = runtime.NewServerMetadataContext(ctx, md)
-		if err != nil {
-			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
-			return
-		}
-
-		forward_TransactionService_TransactionsStateStream_0(ctx, mux, outboundMarshaler, w, req, func() (proto.Message, error) { return resp.Recv() }, mux.GetForwardResponseOptions()...)
-
-	})
-
 	return nil
 }
 
@@ -284,14 +232,10 @@ var (
 	pattern_TransactionService_SubmitTransaction_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"v1", "transaction", "submittransaction"}, "", runtime.AssumeColonVerbOpt(true)))
 
 	pattern_TransactionService_TransactionsState_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"v1", "transaction", "transactionsstate"}, "", runtime.AssumeColonVerbOpt(true)))
-
-	pattern_TransactionService_TransactionsStateStream_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"v1", "transaction", "transactionsstate"}, "", runtime.AssumeColonVerbOpt(true)))
 )
 
 var (
 	forward_TransactionService_SubmitTransaction_0 = runtime.ForwardResponseMessage
 
 	forward_TransactionService_TransactionsState_0 = runtime.ForwardResponseMessage
-
-	forward_TransactionService_TransactionsStateStream_0 = runtime.ForwardResponseStream
 )
