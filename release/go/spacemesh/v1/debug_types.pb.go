126f6e439b95dfb78b6910f4d0723a46313d2126
dmitry <yashulyak@gmail.com>
2022-01-14 16:50:50 +0200

    debug: add networkResponse endpoint with network identity

--- release/go/spacemesh/v1/debug_types.pb.go
+++ release/go/spacemesh/v1/debug_types.pb.go
@@ -72,6 +72,53 @@ func (x *AccountsResponse) GetAccountWrapper() []*Account {
 	return nil
 }
 
+type NetworkInfoResponse struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	Id string `protobuf:"bytes,1,opt,name=id,proto3" json:"id,omitempty"`
+}
+
+func (x *NetworkInfoResponse) Reset() {
+	*x = NetworkInfoResponse{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_spacemesh_v1_debug_types_proto_msgTypes[1]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *NetworkInfoResponse) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*NetworkInfoResponse) ProtoMessage() {}
+
+func (x *NetworkInfoResponse) ProtoReflect() protoreflect.Message {
+	mi := &file_spacemesh_v1_debug_types_proto_msgTypes[1]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use NetworkInfoResponse.ProtoReflect.Descriptor instead.
+func (*NetworkInfoResponse) Descriptor() ([]byte, []int) {
+	return file_spacemesh_v1_debug_types_proto_rawDescGZIP(), []int{1}
+}
+
+func (x *NetworkInfoResponse) GetId() string {
+	if x != nil {
+		return x.Id
+	}
+	return ""
+}
+
 var File_spacemesh_v1_debug_types_proto protoreflect.FileDescriptor
 
 var file_spacemesh_v1_debug_types_proto_rawDesc = []byte{
@@ -85,11 +132,13 @@ var file_spacemesh_v1_debug_types_proto_rawDesc = []byte{
 	0x6f, 0x75, 0x6e, 0x74, 0x5f, 0x77, 0x72, 0x61, 0x70, 0x70, 0x65, 0x72, 0x18, 0x01, 0x20, 0x03,
 	0x28, 0x0b, 0x32, 0x15, 0x2e, 0x73, 0x70, 0x61, 0x63, 0x65, 0x6d, 0x65, 0x73, 0x68, 0x2e, 0x76,
 	0x31, 0x2e, 0x41, 0x63, 0x63, 0x6f, 0x75, 0x6e, 0x74, 0x52, 0x0e, 0x61, 0x63, 0x63, 0x6f, 0x75,
-	0x6e, 0x74, 0x57, 0x72, 0x61, 0x70, 0x70, 0x65, 0x72, 0x42, 0x34, 0x5a, 0x32, 0x67, 0x69, 0x74,
-	0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x73, 0x70, 0x61, 0x63, 0x65, 0x6d, 0x65, 0x73,
-	0x68, 0x6f, 0x73, 0x2f, 0x61, 0x70, 0x69, 0x2f, 0x72, 0x65, 0x6c, 0x65, 0x61, 0x73, 0x65, 0x2f,
-	0x67, 0x6f, 0x2f, 0x73, 0x70, 0x61, 0x63, 0x65, 0x6d, 0x65, 0x73, 0x68, 0x2f, 0x76, 0x31, 0x62,
-	0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+	0x6e, 0x74, 0x57, 0x72, 0x61, 0x70, 0x70, 0x65, 0x72, 0x22, 0x25, 0x0a, 0x13, 0x4e, 0x65, 0x74,
+	0x77, 0x6f, 0x72, 0x6b, 0x49, 0x6e, 0x66, 0x6f, 0x52, 0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65,
+	0x12, 0x0e, 0x0a, 0x02, 0x69, 0x64, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x02, 0x69, 0x64,
+	0x42, 0x34, 0x5a, 0x32, 0x67, 0x69, 0x74, 0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x73,
+	0x70, 0x61, 0x63, 0x65, 0x6d, 0x65, 0x73, 0x68, 0x6f, 0x73, 0x2f, 0x61, 0x70, 0x69, 0x2f, 0x72,
+	0x65, 0x6c, 0x65, 0x61, 0x73, 0x65, 0x2f, 0x67, 0x6f, 0x2f, 0x73, 0x70, 0x61, 0x63, 0x65, 0x6d,
+	0x65, 0x73, 0x68, 0x2f, 0x76, 0x31, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
@@ -104,13 +153,14 @@ func file_spacemesh_v1_debug_types_proto_rawDescGZIP() []byte {
 	return file_spacemesh_v1_debug_types_proto_rawDescData
 }
 
-var file_spacemesh_v1_debug_types_proto_msgTypes = make([]protoimpl.MessageInfo, 1)
+var file_spacemesh_v1_debug_types_proto_msgTypes = make([]protoimpl.MessageInfo, 2)
 var file_spacemesh_v1_debug_types_proto_goTypes = []interface{}{
-	(*AccountsResponse)(nil), // 0: spacemesh.v1.AccountsResponse
-	(*Account)(nil),          // 1: spacemesh.v1.Account
+	(*AccountsResponse)(nil),    // 0: spacemesh.v1.AccountsResponse
+	(*NetworkInfoResponse)(nil), // 1: spacemesh.v1.NetworkInfoResponse
+	(*Account)(nil),             // 2: spacemesh.v1.Account
 }
 var file_spacemesh_v1_debug_types_proto_depIdxs = []int32{
-	1, // 0: spacemesh.v1.AccountsResponse.account_wrapper:type_name -> spacemesh.v1.Account
+	2, // 0: spacemesh.v1.AccountsResponse.account_wrapper:type_name -> spacemesh.v1.Account
 	1, // [1:1] is the sub-list for method output_type
 	1, // [1:1] is the sub-list for method input_type
 	1, // [1:1] is the sub-list for extension type_name
@@ -137,6 +187,18 @@ func file_spacemesh_v1_debug_types_proto_init() {
 				return nil
 			}
 		}
+		file_spacemesh_v1_debug_types_proto_msgTypes[1].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*NetworkInfoResponse); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
 	}
 	type x struct{}
 	out := protoimpl.TypeBuilder{
@@ -144,7 +206,7 @@ func file_spacemesh_v1_debug_types_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_spacemesh_v1_debug_types_proto_rawDesc,
 			NumEnums:      0,
-			NumMessages:   1,
+			NumMessages:   2,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
