9b4d8a2afef7a46fa91b30900e9e1c9adfd31069
Bartosz Różański <bartek.roza@gmail.com>
2022-11-21 11:09:02 +0100

    Add VerifyChallenge to GatewayService

--- release/go/spacemesh/v1/gateway_types.pb.go
+++ release/go/spacemesh/v1/gateway_types.pb.go
@@ -115,6 +115,108 @@ func (x *BroadcastPoetResponse) GetStatus() *status.Status {
 	return nil
 }
 
+type VerifyChallengeRequest struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	Challenge []byte `protobuf:"bytes,1,opt,name=challenge,proto3" json:"challenge,omitempty"`
+	Signature []byte `protobuf:"bytes,2,opt,name=signature,proto3" json:"signature,omitempty"`
+}
+
+func (x *VerifyChallengeRequest) Reset() {
+	*x = VerifyChallengeRequest{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_spacemesh_v1_gateway_types_proto_msgTypes[2]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *VerifyChallengeRequest) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*VerifyChallengeRequest) ProtoMessage() {}
+
+func (x *VerifyChallengeRequest) ProtoReflect() protoreflect.Message {
+	mi := &file_spacemesh_v1_gateway_types_proto_msgTypes[2]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use VerifyChallengeRequest.ProtoReflect.Descriptor instead.
+func (*VerifyChallengeRequest) Descriptor() ([]byte, []int) {
+	return file_spacemesh_v1_gateway_types_proto_rawDescGZIP(), []int{2}
+}
+
+func (x *VerifyChallengeRequest) GetChallenge() []byte {
+	if x != nil {
+		return x.Challenge
+	}
+	return nil
+}
+
+func (x *VerifyChallengeRequest) GetSignature() []byte {
+	if x != nil {
+		return x.Signature
+	}
+	return nil
+}
+
+type VerifyChallengeResponse struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	Hash []byte `protobuf:"bytes,1,opt,name=hash,proto3" json:"hash,omitempty"`
+}
+
+func (x *VerifyChallengeResponse) Reset() {
+	*x = VerifyChallengeResponse{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_spacemesh_v1_gateway_types_proto_msgTypes[3]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *VerifyChallengeResponse) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*VerifyChallengeResponse) ProtoMessage() {}
+
+func (x *VerifyChallengeResponse) ProtoReflect() protoreflect.Message {
+	mi := &file_spacemesh_v1_gateway_types_proto_msgTypes[3]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use VerifyChallengeResponse.ProtoReflect.Descriptor instead.
+func (*VerifyChallengeResponse) Descriptor() ([]byte, []int) {
+	return file_spacemesh_v1_gateway_types_proto_rawDescGZIP(), []int{3}
+}
+
+func (x *VerifyChallengeResponse) GetHash() []byte {
+	if x != nil {
+		return x.Hash
+	}
+	return nil
+}
+
 var File_spacemesh_v1_gateway_types_proto protoreflect.FileDescriptor
 
 var file_spacemesh_v1_gateway_types_proto_rawDesc = []byte{
@@ -129,11 +231,19 @@ var file_spacemesh_v1_gateway_types_proto_rawDesc = []byte{
 	0x73, 0x74, 0x50, 0x6f, 0x65, 0x74, 0x52, 0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65, 0x12, 0x2a,
 	0x0a, 0x06, 0x73, 0x74, 0x61, 0x74, 0x75, 0x73, 0x18, 0x01, 0x20, 0x01, 0x28, 0x0b, 0x32, 0x12,
 	0x2e, 0x67, 0x6f, 0x6f, 0x67, 0x6c, 0x65, 0x2e, 0x72, 0x70, 0x63, 0x2e, 0x53, 0x74, 0x61, 0x74,
-	0x75, 0x73, 0x52, 0x06, 0x73, 0x74, 0x61, 0x74, 0x75, 0x73, 0x42, 0x34, 0x5a, 0x32, 0x67, 0x69,
-	0x74, 0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x73, 0x70, 0x61, 0x63, 0x65, 0x6d, 0x65,
-	0x73, 0x68, 0x6f, 0x73, 0x2f, 0x61, 0x70, 0x69, 0x2f, 0x72, 0x65, 0x6c, 0x65, 0x61, 0x73, 0x65,
-	0x2f, 0x67, 0x6f, 0x2f, 0x73, 0x70, 0x61, 0x63, 0x65, 0x6d, 0x65, 0x73, 0x68, 0x2f, 0x76, 0x31,
-	0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+	0x75, 0x73, 0x52, 0x06, 0x73, 0x74, 0x61, 0x74, 0x75, 0x73, 0x22, 0x54, 0x0a, 0x16, 0x56, 0x65,
+	0x72, 0x69, 0x66, 0x79, 0x43, 0x68, 0x61, 0x6c, 0x6c, 0x65, 0x6e, 0x67, 0x65, 0x52, 0x65, 0x71,
+	0x75, 0x65, 0x73, 0x74, 0x12, 0x1c, 0x0a, 0x09, 0x63, 0x68, 0x61, 0x6c, 0x6c, 0x65, 0x6e, 0x67,
+	0x65, 0x18, 0x01, 0x20, 0x01, 0x28, 0x0c, 0x52, 0x09, 0x63, 0x68, 0x61, 0x6c, 0x6c, 0x65, 0x6e,
+	0x67, 0x65, 0x12, 0x1c, 0x0a, 0x09, 0x73, 0x69, 0x67, 0x6e, 0x61, 0x74, 0x75, 0x72, 0x65, 0x18,
+	0x02, 0x20, 0x01, 0x28, 0x0c, 0x52, 0x09, 0x73, 0x69, 0x67, 0x6e, 0x61, 0x74, 0x75, 0x72, 0x65,
+	0x22, 0x2d, 0x0a, 0x17, 0x56, 0x65, 0x72, 0x69, 0x66, 0x79, 0x43, 0x68, 0x61, 0x6c, 0x6c, 0x65,
+	0x6e, 0x67, 0x65, 0x52, 0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65, 0x12, 0x12, 0x0a, 0x04, 0x68,
+	0x61, 0x73, 0x68, 0x18, 0x01, 0x20, 0x01, 0x28, 0x0c, 0x52, 0x04, 0x68, 0x61, 0x73, 0x68, 0x42,
+	0x34, 0x5a, 0x32, 0x67, 0x69, 0x74, 0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x73, 0x70,
+	0x61, 0x63, 0x65, 0x6d, 0x65, 0x73, 0x68, 0x6f, 0x73, 0x2f, 0x61, 0x70, 0x69, 0x2f, 0x72, 0x65,
+	0x6c, 0x65, 0x61, 0x73, 0x65, 0x2f, 0x67, 0x6f, 0x2f, 0x73, 0x70, 0x61, 0x63, 0x65, 0x6d, 0x65,
+	0x73, 0x68, 0x2f, 0x76, 0x31, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
@@ -148,14 +258,16 @@ func file_spacemesh_v1_gateway_types_proto_rawDescGZIP() []byte {
 	return file_spacemesh_v1_gateway_types_proto_rawDescData
 }
 
-var file_spacemesh_v1_gateway_types_proto_msgTypes = make([]protoimpl.MessageInfo, 2)
+var file_spacemesh_v1_gateway_types_proto_msgTypes = make([]protoimpl.MessageInfo, 4)
 var file_spacemesh_v1_gateway_types_proto_goTypes = []interface{}{
-	(*BroadcastPoetRequest)(nil),  // 0: spacemesh.v1.BroadcastPoetRequest
-	(*BroadcastPoetResponse)(nil), // 1: spacemesh.v1.BroadcastPoetResponse
-	(*status.Status)(nil),         // 2: google.rpc.Status
+	(*BroadcastPoetRequest)(nil),    // 0: spacemesh.v1.BroadcastPoetRequest
+	(*BroadcastPoetResponse)(nil),   // 1: spacemesh.v1.BroadcastPoetResponse
+	(*VerifyChallengeRequest)(nil),  // 2: spacemesh.v1.VerifyChallengeRequest
+	(*VerifyChallengeResponse)(nil), // 3: spacemesh.v1.VerifyChallengeResponse
+	(*status.Status)(nil),           // 4: google.rpc.Status
 }
 var file_spacemesh_v1_gateway_types_proto_depIdxs = []int32{
-	2, // 0: spacemesh.v1.BroadcastPoetResponse.status:type_name -> google.rpc.Status
+	4, // 0: spacemesh.v1.BroadcastPoetResponse.status:type_name -> google.rpc.Status
 	1, // [1:1] is the sub-list for method output_type
 	1, // [1:1] is the sub-list for method input_type
 	1, // [1:1] is the sub-list for extension type_name
@@ -193,6 +305,30 @@ func file_spacemesh_v1_gateway_types_proto_init() {
 				return nil
 			}
 		}
+		file_spacemesh_v1_gateway_types_proto_msgTypes[2].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*VerifyChallengeRequest); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
+		file_spacemesh_v1_gateway_types_proto_msgTypes[3].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*VerifyChallengeResponse); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
 	}
 	type x struct{}
 	out := protoimpl.TypeBuilder{
@@ -200,7 +336,7 @@ func file_spacemesh_v1_gateway_types_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_spacemesh_v1_gateway_types_proto_rawDesc,
 			NumEnums:      0,
-			NumMessages:   2,
+			NumMessages:   4,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
