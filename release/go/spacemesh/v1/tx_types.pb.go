d4cb54e6c592ac04eb7fc6fe120b24fc08460fa5
Dmitry Shulyak <yashulyak@gmail.com>
2023-04-17 08:16:50 +0300

    add endpoint to parse tx without submitting to mempool (#228)
    * add endpoint to parse tx without submitting to mempool
    
    * add option to skip signature verification

--- release/go/spacemesh/v1/tx_types.pb.go
+++ release/go/spacemesh/v1/tx_types.pb.go
@@ -746,6 +746,108 @@ func (x *TransactionResult) GetTouchedAddresses() []string {
 	return nil
 }
 
+type ParseTransactionRequest struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	Transaction []byte `protobuf:"bytes,1,opt,name=transaction,proto3" json:"transaction,omitempty"` // signed binary transaction
+	Verify      bool   `protobuf:"varint,2,opt,name=verify,proto3" json:"verify,omitempty"`          // if true signature verification will be executed
+}
+
+func (x *ParseTransactionRequest) Reset() {
+	*x = ParseTransactionRequest{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_spacemesh_v1_tx_types_proto_msgTypes[10]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *ParseTransactionRequest) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*ParseTransactionRequest) ProtoMessage() {}
+
+func (x *ParseTransactionRequest) ProtoReflect() protoreflect.Message {
+	mi := &file_spacemesh_v1_tx_types_proto_msgTypes[10]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use ParseTransactionRequest.ProtoReflect.Descriptor instead.
+func (*ParseTransactionRequest) Descriptor() ([]byte, []int) {
+	return file_spacemesh_v1_tx_types_proto_rawDescGZIP(), []int{10}
+}
+
+func (x *ParseTransactionRequest) GetTransaction() []byte {
+	if x != nil {
+		return x.Transaction
+	}
+	return nil
+}
+
+func (x *ParseTransactionRequest) GetVerify() bool {
+	if x != nil {
+		return x.Verify
+	}
+	return false
+}
+
+type ParseTransactionResponse struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	Tx *Transaction `protobuf:"bytes,1,opt,name=tx,proto3" json:"tx,omitempty"`
+}
+
+func (x *ParseTransactionResponse) Reset() {
+	*x = ParseTransactionResponse{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_spacemesh_v1_tx_types_proto_msgTypes[11]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *ParseTransactionResponse) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*ParseTransactionResponse) ProtoMessage() {}
+
+func (x *ParseTransactionResponse) ProtoReflect() protoreflect.Message {
+	mi := &file_spacemesh_v1_tx_types_proto_msgTypes[11]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use ParseTransactionResponse.ProtoReflect.Descriptor instead.
+func (*ParseTransactionResponse) Descriptor() ([]byte, []int) {
+	return file_spacemesh_v1_tx_types_proto_rawDescGZIP(), []int{11}
+}
+
+func (x *ParseTransactionResponse) GetTx() *Transaction {
+	if x != nil {
+		return x.Tx
+	}
+	return nil
+}
+
 var File_spacemesh_v1_tx_types_proto protoreflect.FileDescriptor
 
 var file_spacemesh_v1_tx_types_proto_rawDesc = []byte{
@@ -867,11 +969,21 @@ var file_spacemesh_v1_tx_types_proto_rawDesc = []byte{
 	0x64, 0x64, 0x72, 0x65, 0x73, 0x73, 0x65, 0x73, 0x22, 0x2f, 0x0a, 0x06, 0x53, 0x74, 0x61, 0x74,
 	0x75, 0x73, 0x12, 0x0b, 0x0a, 0x07, 0x53, 0x55, 0x43, 0x43, 0x45, 0x53, 0x53, 0x10, 0x00, 0x12,
 	0x0b, 0x0a, 0x07, 0x46, 0x41, 0x49, 0x4c, 0x55, 0x52, 0x45, 0x10, 0x01, 0x12, 0x0b, 0x0a, 0x07,
-	0x49, 0x4e, 0x56, 0x41, 0x4c, 0x49, 0x44, 0x10, 0x02, 0x42, 0x34, 0x5a, 0x32, 0x67, 0x69, 0x74,
-	0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x73, 0x70, 0x61, 0x63, 0x65, 0x6d, 0x65, 0x73,
-	0x68, 0x6f, 0x73, 0x2f, 0x61, 0x70, 0x69, 0x2f, 0x72, 0x65, 0x6c, 0x65, 0x61, 0x73, 0x65, 0x2f,
-	0x67, 0x6f, 0x2f, 0x73, 0x70, 0x61, 0x63, 0x65, 0x6d, 0x65, 0x73, 0x68, 0x2f, 0x76, 0x31, 0x62,
-	0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+	0x49, 0x4e, 0x56, 0x41, 0x4c, 0x49, 0x44, 0x10, 0x02, 0x22, 0x53, 0x0a, 0x17, 0x50, 0x61, 0x72,
+	0x73, 0x65, 0x54, 0x72, 0x61, 0x6e, 0x73, 0x61, 0x63, 0x74, 0x69, 0x6f, 0x6e, 0x52, 0x65, 0x71,
+	0x75, 0x65, 0x73, 0x74, 0x12, 0x20, 0x0a, 0x0b, 0x74, 0x72, 0x61, 0x6e, 0x73, 0x61, 0x63, 0x74,
+	0x69, 0x6f, 0x6e, 0x18, 0x01, 0x20, 0x01, 0x28, 0x0c, 0x52, 0x0b, 0x74, 0x72, 0x61, 0x6e, 0x73,
+	0x61, 0x63, 0x74, 0x69, 0x6f, 0x6e, 0x12, 0x16, 0x0a, 0x06, 0x76, 0x65, 0x72, 0x69, 0x66, 0x79,
+	0x18, 0x02, 0x20, 0x01, 0x28, 0x08, 0x52, 0x06, 0x76, 0x65, 0x72, 0x69, 0x66, 0x79, 0x22, 0x45,
+	0x0a, 0x18, 0x50, 0x61, 0x72, 0x73, 0x65, 0x54, 0x72, 0x61, 0x6e, 0x73, 0x61, 0x63, 0x74, 0x69,
+	0x6f, 0x6e, 0x52, 0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65, 0x12, 0x29, 0x0a, 0x02, 0x74, 0x78,
+	0x18, 0x01, 0x20, 0x01, 0x28, 0x0b, 0x32, 0x19, 0x2e, 0x73, 0x70, 0x61, 0x63, 0x65, 0x6d, 0x65,
+	0x73, 0x68, 0x2e, 0x76, 0x31, 0x2e, 0x54, 0x72, 0x61, 0x6e, 0x73, 0x61, 0x63, 0x74, 0x69, 0x6f,
+	0x6e, 0x52, 0x02, 0x74, 0x78, 0x42, 0x34, 0x5a, 0x32, 0x67, 0x69, 0x74, 0x68, 0x75, 0x62, 0x2e,
+	0x63, 0x6f, 0x6d, 0x2f, 0x73, 0x70, 0x61, 0x63, 0x65, 0x6d, 0x65, 0x73, 0x68, 0x6f, 0x73, 0x2f,
+	0x61, 0x70, 0x69, 0x2f, 0x72, 0x65, 0x6c, 0x65, 0x61, 0x73, 0x65, 0x2f, 0x67, 0x6f, 0x2f, 0x73,
+	0x70, 0x61, 0x63, 0x65, 0x6d, 0x65, 0x73, 0x68, 0x2f, 0x76, 0x31, 0x62, 0x06, 0x70, 0x72, 0x6f,
+	0x74, 0x6f, 0x33,
 }
 
 var (
@@ -887,7 +999,7 @@ func file_spacemesh_v1_tx_types_proto_rawDescGZIP() []byte {
 }
 
 var file_spacemesh_v1_tx_types_proto_enumTypes = make([]protoimpl.EnumInfo, 2)
-var file_spacemesh_v1_tx_types_proto_msgTypes = make([]protoimpl.MessageInfo, 10)
+var file_spacemesh_v1_tx_types_proto_msgTypes = make([]protoimpl.MessageInfo, 12)
 var file_spacemesh_v1_tx_types_proto_goTypes = []interface{}{
 	(TransactionState_TransactionState)(0),  // 0: spacemesh.v1.TransactionState.TransactionState
 	(TransactionResult_Status)(0),           // 1: spacemesh.v1.TransactionResult.Status
@@ -901,29 +1013,32 @@ var file_spacemesh_v1_tx_types_proto_goTypes = []interface{}{
 	(*TransactionState)(nil),                // 9: spacemesh.v1.TransactionState
 	(*TransactionResultsRequest)(nil),       // 10: spacemesh.v1.TransactionResultsRequest
 	(*TransactionResult)(nil),               // 11: spacemesh.v1.TransactionResult
-	(*TransactionId)(nil),                   // 12: spacemesh.v1.TransactionId
-	(*status.Status)(nil),                   // 13: google.rpc.Status
-	(*Transaction)(nil),                     // 14: spacemesh.v1.Transaction
+	(*ParseTransactionRequest)(nil),         // 12: spacemesh.v1.ParseTransactionRequest
+	(*ParseTransactionResponse)(nil),        // 13: spacemesh.v1.ParseTransactionResponse
+	(*TransactionId)(nil),                   // 14: spacemesh.v1.TransactionId
+	(*status.Status)(nil),                   // 15: google.rpc.Status
+	(*Transaction)(nil),                     // 16: spacemesh.v1.Transaction
 }
 var file_spacemesh_v1_tx_types_proto_depIdxs = []int32{
-	12, // 0: spacemesh.v1.TransactionsIds.transaction_id:type_name -> spacemesh.v1.TransactionId
-	13, // 1: spacemesh.v1.SubmitTransactionResponse.status:type_name -> google.rpc.Status
+	14, // 0: spacemesh.v1.TransactionsIds.transaction_id:type_name -> spacemesh.v1.TransactionId
+	15, // 1: spacemesh.v1.SubmitTransactionResponse.status:type_name -> google.rpc.Status
 	9,  // 2: spacemesh.v1.SubmitTransactionResponse.txstate:type_name -> spacemesh.v1.TransactionState
-	12, // 3: spacemesh.v1.TransactionsStateRequest.transaction_id:type_name -> spacemesh.v1.TransactionId
+	14, // 3: spacemesh.v1.TransactionsStateRequest.transaction_id:type_name -> spacemesh.v1.TransactionId
 	9,  // 4: spacemesh.v1.TransactionsStateResponse.transactions_state:type_name -> spacemesh.v1.TransactionState
-	14, // 5: spacemesh.v1.TransactionsStateResponse.transactions:type_name -> spacemesh.v1.Transaction
-	12, // 6: spacemesh.v1.TransactionsStateStreamRequest.transaction_id:type_name -> spacemesh.v1.TransactionId
+	16, // 5: spacemesh.v1.TransactionsStateResponse.transactions:type_name -> spacemesh.v1.Transaction
+	14, // 6: spacemesh.v1.TransactionsStateStreamRequest.transaction_id:type_name -> spacemesh.v1.TransactionId
 	9,  // 7: spacemesh.v1.TransactionsStateStreamResponse.transaction_state:type_name -> spacemesh.v1.TransactionState
-	14, // 8: spacemesh.v1.TransactionsStateStreamResponse.transaction:type_name -> spacemesh.v1.Transaction
-	12, // 9: spacemesh.v1.TransactionState.id:type_name -> spacemesh.v1.TransactionId
+	16, // 8: spacemesh.v1.TransactionsStateStreamResponse.transaction:type_name -> spacemesh.v1.Transaction
+	14, // 9: spacemesh.v1.TransactionState.id:type_name -> spacemesh.v1.TransactionId
 	0,  // 10: spacemesh.v1.TransactionState.state:type_name -> spacemesh.v1.TransactionState.TransactionState
-	14, // 11: spacemesh.v1.TransactionResult.tx:type_name -> spacemesh.v1.Transaction
+	16, // 11: spacemesh.v1.TransactionResult.tx:type_name -> spacemesh.v1.Transaction
 	1,  // 12: spacemesh.v1.TransactionResult.status:type_name -> spacemesh.v1.TransactionResult.Status
-	13, // [13:13] is the sub-list for method output_type
-	13, // [13:13] is the sub-list for method input_type
-	13, // [13:13] is the sub-list for extension type_name
-	13, // [13:13] is the sub-list for extension extendee
-	0,  // [0:13] is the sub-list for field type_name
+	16, // 13: spacemesh.v1.ParseTransactionResponse.tx:type_name -> spacemesh.v1.Transaction
+	14, // [14:14] is the sub-list for method output_type
+	14, // [14:14] is the sub-list for method input_type
+	14, // [14:14] is the sub-list for extension type_name
+	14, // [14:14] is the sub-list for extension extendee
+	0,  // [0:14] is the sub-list for field type_name
 }
 
 func init() { file_spacemesh_v1_tx_types_proto_init() }
@@ -1053,6 +1168,30 @@ func file_spacemesh_v1_tx_types_proto_init() {
 				return nil
 			}
 		}
+		file_spacemesh_v1_tx_types_proto_msgTypes[10].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*ParseTransactionRequest); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
+		file_spacemesh_v1_tx_types_proto_msgTypes[11].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*ParseTransactionResponse); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
 	}
 	type x struct{}
 	out := protoimpl.TypeBuilder{
@@ -1060,7 +1199,7 @@ func file_spacemesh_v1_tx_types_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_spacemesh_v1_tx_types_proto_rawDesc,
 			NumEnums:      2,
-			NumMessages:   10,
+			NumMessages:   12,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
