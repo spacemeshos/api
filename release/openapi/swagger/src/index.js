f1fdcdf95e277f9c6edaa9cfcadcbcc810630395
andres-spacemesh <106999199+andres-spacemesh@users.noreply.github.com>
2024-06-14 08:17:13 -0700

    add swagger-ui

---
+++ release/openapi/swagger/src/index.js
@@ -0,0 +1,7 @@
+const SwaggerUI = require('swagger-ui');
+require('swagger-ui/dist/swagger-ui.css');
+const spec = require('./api.swagger.json');
+SwaggerUI({
+    spec,
+    dom_id: '#swagger',
+});
